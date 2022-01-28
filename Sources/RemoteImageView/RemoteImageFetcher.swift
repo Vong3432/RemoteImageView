5648586e8747a3f948f1990762018eab087f71b0
Vong3432 <vongnyuksoon2000@gmail.com>
2022-01-28 16:05:31 +0800

    Initial Commit

---
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vong Nyuksoon on 28/01/2022.
+//
+
+import Foundation
+import SwiftUI
+
+public class RemoteImageFetcher: ObservableObject {
+  @Published var imageData = Data()
+  let url: URL
+
+  public init(url: URL) {
+    self.url = url
+  }
+
+  // 1
+  public func fetch() {
+    URLSession.shared.dataTask(with: url) { (data, _, _) in
+      guard let data = data else { return }
+      DispatchQueue.main.async {
+        self.imageData = data
+      }
+    }.resume()
+  }
+
+  // 2
+  public func getImageData() -> Data {
+    return imageData
+  }
+
+  // 3
+  public func getUrl() -> URL {
+    return url
+  }
+}
